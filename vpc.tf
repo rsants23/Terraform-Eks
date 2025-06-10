731cc474839f8c448033ff9898e7e13d59b8b464
Rodolfo Santos <rodolfosantos@r.santos>
2025-06-09 22:01:00 -0300

    break

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "eks_vpn" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
-  enable_dns_hostnames = true
+  enable_dns_hostnames = tru
   tags = {
     Name = "comunidadedevops-vpc"
   }
