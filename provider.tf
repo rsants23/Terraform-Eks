8b29b7d9c0cbcc903d9c9b3499a66afdc1ad785d
Rodolfo Santos <rodolfosantos@r.santos>
2025-06-09 21:50:23 -0300

    fix: lint

--- provider.tf
+++ provider.tf
@@ -1,9 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.99.1"
     }
+  }
+  backend "s3" {
+    bucket = "terraform-comunidadedevops"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
