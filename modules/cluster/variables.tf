fe15b76093cd04cba807341bfe5dd168657456a8
Rodolfo Santos <rodolfo.s.santos@hotmail.com>
2025-06-19 16:13:49 -0300

    feat: new cluster module

--- modules/cluster/variables.tf
+++ modules/cluster/variables.tf
@@ -12,3 +12,13 @@ variable "tags" {
   type        = map(any)
   description = "Tags to be added to AWS resources"
 }
+
+variable "public_subnet_1a" {
+  type        = string
+  description = "Subnet to created EKS cluester AZ 1a"
+}
+
+variable "public_subnet_1b" {
+  type        = string
+  description = "Subnet to created EKS cluester AZ 1b"
+}
