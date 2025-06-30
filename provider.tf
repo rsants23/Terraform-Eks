6db3ffe86fad49557fbb146617a51f37a42b6b16
Rodolfo Santos <rodolfo.s.santos@hotmail.com>
2025-06-29 23:09:32 -0300

    feat: modify modules, output, provider

--- provider.tf
+++ provider.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.99.1"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.37.1"
+    }
   }
   backend "s3" {
     bucket = "terraform-comunidadedevops"
@@ -14,4 +18,14 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+}
+
+provider "kubernetes" {
+  host                   = module.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+    command     = "aws"
+  }
 }
